9b7c82cede139e78e96acb6c7b75aa40b6ce2539
yashsapariya <yashsapariya203@gmail.com>
2025-01-22 11:09:00 +0530

    React demo

---
+++ LAB-13 (From React)/LAB-13/src/ClassComponent.jsx
@@ -0,0 +1,16 @@
+import {Component} from "react";
+
+class ClassComponent extends Component{
+
+    render(){
+        return(
+            <>
+                <div>This is a class Component</div>
+
+                <h1>Hellow</h1>
+            </>
+        )
+    }
+}
+
+export default ClassComponent;
